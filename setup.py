da0b80d00f390aea4d5c6159a77a93433ea17ff8
Matthew Newman <newman99@gmail.com>
2018-11-09 15:20:43 -0500

    Fixed createsuperuser - fixed project_name.

--- setup.py
+++ setup.py
@@ -115,8 +115,8 @@ def main(project_name, name, email, password, aws, build, buildall,
                 'web',
                 '/bin/bash',
                 '-c',
-                '''/var/task/test3/manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('{}', '{}', '{}')"'''.format(  # noqa
-                    name, email, password
+                '''/var/task/{}/manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('{}', '{}', '{}')"'''.format(  # noqa
+                    project_name, name, email, password
                 )
             ])
             subprocess.run([
